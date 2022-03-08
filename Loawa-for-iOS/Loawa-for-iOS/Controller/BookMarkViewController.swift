21287ef64a800bf7b2a53e2fc26d2c719a747b6a
doogie97 <chris_97@naver.com>
2022-03-08 18:48:34 +0900

    feat: table뷰 추가 및 북마크로 이동시 목록에 등록된 유저 이름 표시

--- Loawa-for-iOS/Loawa-for-iOS/Controller/BookMarkViewController.swift
+++ Loawa-for-iOS/Loawa-for-iOS/Controller/BookMarkViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class BookMarkViewController: UIViewController {
+    @IBOutlet weak var bookmarkTableView: UITableView!
+    let cellName = "BookmarkTableViewCell"
     var userNames : [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bookmarkTableView.delegate = self
+        bookmarkTableView.dataSource = self
     }
-    
     //MARK: - IBActions
     @IBAction func touchCloseButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension BookMarkViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let bookmarkTableViewCell = bookmarkTableView.dequeueReusableCell(withIdentifier: "BookmarkTableViewCell", for: indexPath) as? BookmarkTableViewCell else { return }
+        let bookmarkTableViewCell = bookmarkTableView.dequeueReusableCell(withIdentifier: "BookmarkTableViewCell", for: indexPath)
+        guard let bookmarkTableViewCell = bookmarkTableViewCell as? BookmarkTableViewCell else { return bookmarkTableViewCell }
+        bookmarkTableViewCell.userNameLabel.text = self.userNames[indexPath.row]
+        
+        return bookmarkTableViewCell
     }
 }
---
+++ Loawa-for-iOS/Loawa-for-iOS/Controller/BookmarkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookmarkTableViewCell.swift
+//  Loawa-for-iOS
+//
+//  Created by 최최성균 on 2022/03/08.
+//
+
+import UIKit
+
+class BookmarkTableViewCell: UITableViewCell {
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
